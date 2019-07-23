b76054713a4520d3e770141fe2ebabae69d3c2a9
Brice Texier <brice@codeur.com>
2019-07-23 11:56:21 +0200

    Revert "Fix tests"
    This reverts commit 037b11bb814ea21b54866ab131868b7c2437c32b.

--- lib/active_merchant/billing/paybox_direct_plus_gateway.rb
+++ lib/active_merchant/billing/paybox_direct_plus_gateway.rb
@@ -5,47 +5,25 @@ require 'active_merchant'
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     class PayboxDirectPlusGateway < Gateway
-      mattr_accessor :test_backup_url, :live_backup_url
-
-      self.test_url        = 'https://preprod-ppps.paybox.com/PPPS.php'
-      self.test_backup_url = 'https://preprod-ppps.paybox.com/PPPS.php'
-      self.live_url        = 'https://ppps.paybox.com/PPPS.php'
-      self.live_backup_url = 'https://ppps1.paybox.com/PPPS.php'
-
-      self.supported_countries = ['FR']
-      self.default_currency = 'EUR'
-      self.supported_cardtypes = [:visa, :master, :american_express, :diners_club, :jcb]
-
-      self.homepage_url = 'http://www.paybox.com/'
-      self.display_name = 'Paybox Direct Plus'
-
-      self.money_format = :cents
+      TEST_URL = 'https://preprod-ppps.paybox.com/PPPS.php'
+      TEST_URL_BACKUP = 'https://preprod-ppps.paybox.com/PPPS.php'
+      LIVE_URL = 'https://ppps.paybox.com/PPPS.php'
+      LIVE_URL_BACKUP = 'https://ppps1.paybox.com/PPPS.php'
 
       # Payment API Version
-      # 00103 for Paybox Direct
-      # 00104 for Paybox Direct Plus
       API_VERSION = '00104'
 
       # Transactions hash
       TRANSACTIONS = {
-        authorize:              '00001',
-        capture:                '00002',
-        purchase:               '00003',
-        credit:                 '00004',
-        void:                   '00005',
-        verify:                 '00011',
-        capture_directly:       '00012', # Capture without authorization
-        refund:                 '00014',
-        consultation:           '00017',
-        subscriber_authorize:   '00051',
-        subscriber_capture:     '00052',
-        subscriber_purchase:    '00053',
-        subscriber_credit:      '00054',
-        subscriber_void:        '00055',
-        subscriber_create:      '00056',
-        subscriber_update:      '00057',
-        subscriber_destroy:     '00058',
-        force_capture_directly: '00061'
+        subscriber_authorization: '00051',
+        subscriber_capture:       '00052',
+        subscriber_purchase:      '00053',
+        subscriber_credit:        '00054',
+        subscriber_void:          '00055',
+        subscriber_create:        '00056',
+        subscriber_update:        '00057',
+        subscriber_destroy:       '00058',
+        subscriber_refund:        '00014'
       }.freeze
 
       CURRENCY_CODES = {
@@ -65,40 +43,6 @@ module ActiveMerchant #:nodoc:
         'EUR' => '978'
       }.freeze
 
-      # STANDARD_ERROR_CODE_MAPPING = {
-      #   '00000' => STANDARD_ERROR_CODE[], # Opération réussie.
-      #   '00001' => STANDARD_ERROR_CODE[], # La connexion au centre d’autorisation a échoué ou une erreur interne est survenue. Dans ce cas, il est souhaitable de faire une tentative sur le site secondaire : ppps1.paybox.com.
-      #   "'001XX" # Paiement refusé par le centre d’autorisation.[voir§9.1.1Réseaux CB, Visa, Mastercard, American Express et Diners]En cas d’autorisation de la transaction par le centre d’autorisation de la banque, le résultat “00100” sera en fait remplacé directement par “00000”.
-      #   '00002' => STANDARD_ERROR_CODE[], # Une erreur de cohérence est survenue.
-      #   '00003' => STANDARD_ERROR_CODE[], # Erreur Paybox. Dans ce cas, il est souhaitable de faire une tentative sur le site secondaire : ppps1.paybox.com.
-      #   '00004' => STANDARD_ERROR_CODE[:invalid_number], # Numéro de porteur invalide.
-      #   '00005' => STANDARD_ERROR_CODE[], # Numéro de question invalide.
-      #   '00006' => STANDARD_ERROR_CODE[], # Accès refusé ou site / rang incorrect.
-      #   '00007' => STANDARD_ERROR_CODE[], # Date invalide.
-      #   '00008' => STANDARD_ERROR_CODE[:invalid_expiry_date], # Date de fin de validité incorrecte.
-      #   '00009' => STANDARD_ERROR_CODE[], # Type d’opération invalide.
-      #   '00010' => STANDARD_ERROR_CODE[], # Devise inconnue.
-      #   '00011' => STANDARD_ERROR_CODE[], # Montant incorrect.
-      #   '00012' => STANDARD_ERROR_CODE[], # Référence commande invalide.
-      #   '00013' => STANDARD_ERROR_CODE[], # Cette version n’est plus soutenue.
-      #   '00014' => STANDARD_ERROR_CODE[], # Trame reçue incohérente.
-      #   '00015' => STANDARD_ERROR_CODE[], # Erreur d’accès aux données précédemment référencées.
-      #   '00016' => STANDARD_ERROR_CODE[], # Abonné déjà existant (inscription nouvel abonné).
-      #   '00017' => STANDARD_ERROR_CODE[], # Abonné inexistant.
-      #   '00018' => STANDARD_ERROR_CODE[], # Transaction non trouvée (question du type 11).
-      #   # '00019' => STANDARD_ERROR_CODE[], # Réservé.
-      #   '00020' => STANDARD_ERROR_CODE[:invalid_cvc], # Cryptogramme visuel non présent.
-      #   '00021' => STANDARD_ERROR_CODE[:card_declined], # Carte non autorisée.
-      #   '00022' => STANDARD_ERROR_CODE[], # Plafond atteint.
-      #   '00023' => STANDARD_ERROR_CODE[], # Porteur déjà passé aujourd’hui.
-      #   '00024' => STANDARD_ERROR_CODE[], # Code pays filtré pour ce commerçant.
-      #   '00026' => STANDARD_ERROR_CODE[], # Code activité incorrect.
-      #   '00040' => STANDARD_ERROR_CODE[], # Porteur enrôlé mais non authentifié.
-      #   '00097' => STANDARD_ERROR_CODE[], # Timeout de connexion atteint.
-      #   '00098' => STANDARD_ERROR_CODE[], # Erreur de connexion interne.
-      #   '00099' => STANDARD_ERROR_CODE[], # Incohérence entre la question et la réponse. Refaire une nouvelle tentative ultérieurement.
-      # }.freeze
-
       ALREADY_EXISTING_PROFILE_CODES = ['00016'].freeze
       UNKNOWN_PROFILE_CODES = ['00017'].freeze
       SUCCESS_CODES = ['00000'].freeze
@@ -107,33 +51,50 @@ module ActiveMerchant #:nodoc:
       SUCCESS_MESSAGE = 'The transaction was approved'
       FAILURE_MESSAGE = 'The transaction failed'
 
+      # Money is referenced in cents
+      self.money_format = :cents
+      self.default_currency = 'EUR'
+
+      # The countries the gateway supports merchants from as 2 digit ISO country codes
+      self.supported_countries = ['FR']
+
+      # The card types supported by the payment gateway
+      self.supported_cardtypes = [:visa, :master, :american_express, :diners_club, :jcb]
+
+      # The homepage URL of the gateway
+      self.homepage_url = 'http://www.paybox.com/'
+
+      # The name of the gateway
+      self.display_name = 'Paybox Direct Plus'
+
       def initialize(options = {})
         requires!(options, :login, :password)
-        @site = options[:login].to_s[0, 7]
-        @rang = options[:login].to_s[7..-1]
-        @cle  = options[:password]
+        @options = options
         super
       end
 
-      def purchase(money, payment, options = {})
-        requires!(options, :credit_card_reference, :user_reference)
-        post = {}
-        add_invoice(post, options)
-        add_credit_card(post, payment, options)
-        add_user_reference(post, options)
-        add_test_error_code(post, options)
-        commit('subscriber_purchase', money, post)
+      def payment_profiles_supported?
+        true
       end
 
-      def authorize(money, payment, options = {})
+      def authorize(money, creditcard, options = {})
         requires!(options, :user_reference)
         post = {}
         add_invoice(post, options)
-        add_credit_card(post, payment, options)
+        add_creditcard(post, creditcard, options)
         add_user_reference(post, options)
         add_test_error_code(post, options)
+        commit('subscriber_authorization', money, post)
+      end
 
-        commit('subscriber_authorize', money, post)
+      def purchase(money, creditcard, options = {})
+        requires!(options, :credit_card_reference, :user_reference)
+        post = {}
+        add_invoice(post, options)
+        add_creditcard(post, creditcard, options)
+        add_user_reference(post, options)
+        add_test_error_code(post, options)
+        commit('subscriber_purchase', money, post)
       end
 
       def capture(money, authorization, options = {})
@@ -151,11 +112,10 @@ module ActiveMerchant #:nodoc:
         add_invoice(post, options)
         add_reference(post, authorization)
         add_user_reference(post, options)
-        commit('refund', money, post)
+        commit('subscriber_refund', money, post)
       end
 
       def void(money, authorization, options = {})
-        # def void(authorization, options = {})
         requires!(options, :order_id, :user_reference)
         post = {}
         add_invoice(post, options)
@@ -164,13 +124,6 @@ module ActiveMerchant #:nodoc:
         post[:porteur] = '000000000000000'
         post[:dateval] = '0000'
         commit('subscriber_void', money, post)
-      end
-
-      def verify(credit_card, options = {})
-        MultiResponse.run(:use_first_response) do |r|
-          r.process { authorize(100, credit_card, options) }
-          r.process(:ignore_result) { void(r.authorization, options) }
-        end
       end
 
       def credit(money, identification, options = {})
@@ -182,23 +135,19 @@ module ActiveMerchant #:nodoc:
         commit('subscriber_credit', money, post)
       end
 
-      def payment_profiles_supported?
-        true
-      end
-
-      def create_payment_profile(money, credit_card, options = {})
+      def create_payment_profile(money, creditcard, options = {})
         requires!(options, :user_reference)
         post = {}
         add_invoice(post, options)
-        add_credit_card(post, credit_card, options)
+        add_creditcard(post, creditcard, options)
         add_user_reference(post, options)
         add_test_error_code(post, options)
         commit('subscriber_create', money, post)
       end
 
-      def update_payment_profile(money, credit_card, options = {})
+      def update_payment_profile(money, creditcard, options = {})
         post = {}
-        add_credit_card(post, credit_card, options)
+        add_creditcard(post, creditcard, options)
         add_user_reference(post, options)
         add_test_error_code(post, options)
         commit('subscriber_update', money, post)
@@ -211,13 +160,10 @@ module ActiveMerchant #:nodoc:
         commit('subscriber_destroy', money, post)
       end
 
-      def supports_scrubbing?
-        true
-      end
-
-      def scrub(transcript)
-        transcript
-          .gsub(/\b(CLE|PORTEUR|CVV|DATENAISS)=\w+/, '\1=[FILTERED]')
+      def test?
+        # Rails.logger.info "====================TEST MODE ??========================"
+        # Rails.logger.info "#{@options[:test] || Base.gateway_mode == :test}"
+        @options[:test] || Base.gateway_mode == :test
       end
 
     private
@@ -226,10 +172,10 @@ module ActiveMerchant #:nodoc:
         post[:reference] = options[:order_id]
       end
 
-      def add_credit_card(post, credit_card, options = {})
-        post[:porteur] = options[:credit_card_reference] || credit_card.number
-        post[:dateval] = expdate(credit_card)
-        post[:cvv] = credit_card.verification_value if credit_card.verification_value?
+      def add_creditcard(post, creditcard, options = {})
+        post[:porteur] = options[:credit_card_reference] || creditcard.number
+        post[:dateval] = expdate(creditcard)
+        post[:cvv] = creditcard.verification_value if creditcard.verification_value?
       end
 
       def add_user_reference(post, options)
@@ -246,43 +192,40 @@ module ActiveMerchant #:nodoc:
       end
 
       def parse(body)
-        body.encode!('UTF-8', 'ISO8859-1')
+        body.encode!(Encoding::ISO_8859_1, invalid: :replace, undef: :replace, replace: '?')
         results = {}
         body.split(/&/).each do |pair|
           key, val = pair.split(/=/)
           results[key.downcase.to_sym] = CGI.unescape(val) if val
         end
-        results[:credit_card_reference] = results[:porteur] if results[:porteur]
+        # Rails.logger.info results.inspect
         results
       end
 
       def commit(action, money = nil, parameters = nil)
         parameters[:montant] = ('0000000000' + (money ? amount(money) : ''))[-10..-1]
         parameters[:devise] = CURRENCY_CODES[options[:currency] || currency(money)]
-        url = (test? ? test_url : live_url)
-        response = parse(ssl_post(url, post_data(action, parameters)))
+        request_data = post_data(action, parameters)
+        response = parse(ssl_post(test? ? TEST_URL : LIVE_URL, request_data))
         if service_unavailable?(response)
-          backup_url = (test? ? test_backup_url : live_backup_url)
-          response = parse(ssl_post(backup_url, post_data(action, parameters)))
+          request_data = post_data(action, parameters)
+          response = parse(ssl_post(test? ? TEST_URL_BACKUP : LIVE_URL_BACKUP, request_data))
         end
-        success = success_from(response)
         Response.new(
-          success,
+          success?(response),
           message_from(response),
-          response,
-          # response.merge(
-          #   timestamp: parameters[:dateq],
-          #   # sent_params: parameters.delete_if { |key, _value| %w[porteur dateval cvv].include?(key.to_s) }
-          #   # query:     request_data
-          # ),
+          response.merge(
+            timestamp:   parameters[:dateq],
+            sent_params: parameters.delete_if { |key, _value| %w[porteur dateval cvv].include?(key.to_s) }
+          ),
           test:          test?,
           authorization: response[:numappel].to_s + response[:numtrans].to_s,
           fraud_review:  fraud_review?(response),
-          error_code:    success ? nil : response[:codereponse]
+          sent_params:   parameters.delete_if { |key, _value| %w[porteur dateval cvv].include?(key.to_s) }
         )
       end
 
-      def success_from(response)
+      def success?(response)
         SUCCESS_CODES.include?(response[:codereponse])
       end
 
@@ -303,7 +246,7 @@ module ActiveMerchant #:nodoc:
       end
 
       def message_from(response)
-        success_from(response) ? SUCCESS_MESSAGE : (response[:commentaire] || FAILURE_MESSAGE)
+        success?(response) ? SUCCESS_MESSAGE : (response[:commentaire] || FAILURE_MESSAGE)
       end
 
       def post_data(action, parameters = {})
@@ -312,21 +255,21 @@ module ActiveMerchant #:nodoc:
           type:        TRANSACTIONS[action.to_sym],
           dateq:       Time.now.strftime('%d%m%Y%H%M%S'),
           numquestion: unique_id(parameters[:reference]),
-          site:        @site,
-          rang:        @rang,
-          cle:         @cle,
+          site:        @options[:login].to_s[0, 7],
+          rang:        @options[:login].to_s[7..-1],
+          cle:         @options[:password],
           pays:        '',
           archivage:   parameters[:reference],
           activite:    '027'
         )
 
-        query = parameters.collect { |key, value| "#{key.to_s.upcase}=#{CGI.escape(value.to_s)}" }.join('&')
+        p = parameters.collect { |key, value| "#{key.to_s.upcase}=#{CGI.escape(value.to_s)}" }.join('&')
         # Rails.logger.info "\n***************************"
         # Rails.logger.debug "********** POST DATA IN PAYBOX PLUS ***********"
         # Rails.logger.debug "*** Parameters for post data:"
-        # Rails.logger.debug "#{query.inspect}"
+        # Rails.logger.debug "#{p.inspect}"
         # Rails.logger.info "*****************************"
-        query
+        p
       end
 
       def unique_id(seed = 0)
@@ -336,8 +279,8 @@ module ActiveMerchant #:nodoc:
       end
 
       def expdate(credit_card)
-        year  = Kernel.format('%.4i', credit_card.year)
-        month = Kernel.format('%.2i', credit_card.month)
+        year  = format('%.4i', credit_card.year)
+        month = format('%.2i', credit_card.month)
 
         "#{month}#{year[-2..-1]}"
       end
--- paybox_direct_plus.gemspec
+++ paybox_direct_plus.gemspec
@@ -37,12 +37,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
-  spec.add_dependency 'activemerchant', '~> 1.0'
-  spec.add_development_dependency 'actionpack'
-  spec.add_development_dependency 'activesupport'
+  spec.add_dependency 'activemerchant', '~> 1.10'
+  spec.add_development_dependency 'active_support', '>= 5'
   spec.add_development_dependency 'bundler', '~> 1.17'
   spec.add_development_dependency 'minitest', '~> 5.0'
-  spec.add_development_dependency 'mocha'
-  spec.add_development_dependency 'money'
-  spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rake', '>= 10'
+  spec.add_development_dependency 'shoulda', '>= 0'
 end
